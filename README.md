a1e53a253226a60e90f3686286b84f28fd80354e
Lynn <lynn@fidmail.com>
2018-03-05 14:01:37 -0600

    update metadata

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 # scalr-jenkins
 
-TODO: Enter the cookbook description here.
+Cookbook used to stand up a base Jenkins Installs
 
+
+# Base databag is scalr-jenkins
+
+ex)
+{
+  "id":"chef",
+  "private_key":"-----BEGIN RSA PRIVATE KEY-----\nMIIEowIqc9uvCHngP\n-----END RSA PRIVATE KEY-----",
+  "public_keys":"ssh-rsa AAAAB3NzaC1yc2EAAAAuELxYQ==",
+  "password":"xxxxx"
+}
--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
 name 'scalr-jenkins'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
+maintainer 'lynn@scalr.com'
+maintainer_email 'lynn@scalr.com'
 license 'All Rights Reserved'
 description 'Installs/Configures scalr-jenkins'
 long_description 'Installs/Configures scalr-jenkins'
-version '0.1.0'
+version '0.1.1'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
 issues_url 'https://github.com/lcc2207/scalr-jenkins/issues'
