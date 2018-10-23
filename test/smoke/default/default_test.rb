16062c5540e7c9e893d131e2de359fdd55a2829b
lcc2207 <lynn@fidmail.com>
2018-10-23 11:00:46 -0500

    lint

--- test/smoke/default/default_test.rb
+++ test/smoke/default/default_test.rb
@@ -1,18 +1,20 @@
-# # encoding: utf-8
-
-# Inspec test for recipe scalr-jenkins::default
-
-# The Inspec reference, with examples and extensive documentation, can be
-# found at http://inspec.io/docs/reference/resources/
 
 unless os.windows?
-  # This is an example test, replace with your own test.
-  describe user('root'), :skip do
+  describe user('root') do
     it { should exist }
   end
 end
 
-# This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+describe port(8080) do
+  it { should be_listening }
+end
+
+%w(jenkins ntp).each do |pkg|
+  describe package(pkg) do
+    it { should be_installed }
+  end
+end
+
+describe directory('/etc/jenkins_jobs') do
+  it { should exist }
 end
